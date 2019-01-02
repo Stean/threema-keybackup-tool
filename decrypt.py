3c0d1ecd97f9fed4c39d31a667242c3572fca012
Stean <Stean@users.noreply.github.com>
2019-01-02 23:17:28 +0100

    Switch to library which supports XSalsa20 and rework code structure

--- decrypt.py
+++ decrypt.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+"""
+Author: Stean
+Description:
+This script is based on Threema's official whitepaper (https://threema.ch/press-files/cryptography_whitepaper.pdf) and allows to check, whether a given backup key + password is correct and is able to extract the encoded information.
+"""
+
 import base64
-import Crypto.Hash.SHA256
+import hashlib
 import logging
-from Crypto.Cipher import Salsa20
-from Crypto.Protocol.KDF import PBKDF2
-from Crypto.Hash import SHA256
+import salsa20
+import binascii
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -22,7 +27,7 @@ def decode_backup_code(dashed_input):
     return encrypted_keyinput
 
 def derive_key_decryption_key(password, salt):
-    return PBKDF2(password, salt, dkLen=32, count=100000, hmac_hash_module=Crypto.Hash.SHA256)
+    return hashlib.pbkdf2_hmac("sha256", password, salt, 100000, dklen=32)
 
 
 def decrypt_key(encrypted_keyinput, password):
@@ -35,44 +40,51 @@ def decrypt_key(encrypted_keyinput, password):
     key_decryption_key = derive_key_decryption_key(password, salt)
     logger.info("[decrypt_key] derived key_decryption_key: %s", repr(key_decryption_key))
 
-    cipher = Salsa20.new(key=key_decryption_key, nonce="\x00"*8)
-    decrypted_keyinfo = cipher.decrypt(ciphertext)
+    decrypted_keyinfo = salsa20.XSalsa20_xor(ciphertext, "\x00"*24, key_decryption_key)
 
     return decrypted_keyinfo
 
-def verify_keyinfo(decrypted_keyinfo):
-    logger.debug("[verify_keyinfo] len(decrypted_keyinfo): %d", len(decrypted_keyinfo))
+def parse_keyinfo(decrypted_keyinfo):
     assert len(decrypted_keyinfo)==42, "decrypted_keyinfo has invalid length of " + len(decrypted_keyinfo) + " instead of 42"
 
-    identity    = decrypted_keyinfo[:8]     # 8 character Threema ID
-    private_key = decrypted_keyinfo[8:8+32] # 32 byte private key
-    verification_hash_bytes = decrypted_keyinfo[-2:] # the first two bytes of the resulting hash are used during restoration to verify with reasonable con dence that the provided password was correct
+    result = dict()
 
-    logger.info("identity: %s", repr(identity))
-    logger.info("private_key: %s", repr(private_key))
-    logger.info("verification_hash_bytes: %s", repr(verification_hash_bytes))
+    result["identity"]    = decrypted_keyinfo[:8]     # 8 character Threema ID
+    result["private_key"] = decrypted_keyinfo[8:8+32] # 32 byte private key
+    result["verification_bytes"] = decrypted_keyinfo[-2:] # the first two bytes of the resulting hash are used during restoration to verify with reasonable con dence that the provided password was correct
 
+    return result
+
+def verify_keyinfo(keyinfo_dict):
     # Calc hash
-    h = SHA256.new()
-    h.update(identity + private_key)
+    h = hashlib.sha256()
+    h.update(keyinfo_dict["identity"])      # Feed in ID
+    h.update(keyinfo_dict["private_key"])   # Feed in private Key
     logger.debug("SHA256(identity + private_key): %s", h.hexdigest())
 
-    return h.digest()[:2] == verification_hash_bytes
+    return h.digest()[:2] == keyinfo_dict["verification_bytes"]
 
+def print_keyinfo_dict(keyinfo_dict):
+    print("============ Result ============")
+    print("identity: %s" % keyinfo_dict["identity"])
+    print("private key (bin): %s" % repr(keyinfo_dict["private_key"]))
+    print("private key (hex): %s" % binascii.hexlify(keyinfo_dict["private_key"]))
+    print("verification hash bytes: %s" % repr(keyinfo_dict["verification_bytes"]))
 
 ##### Main
-backup_key = "XXX"
-backup_key_password = "12345678"
+backup_key = "<insert your backup key here>"
+backup_key_password = "<insert your backup password here>"
 
 encrypted_keyinput = decode_backup_code(backup_key)
 logger.info("decoded backup code: %s", repr(encrypted_keyinput))
 
 decrypted_keyinfo = decrypt_key(encrypted_keyinput, backup_key_password)
-
 logger.info("decrypted keyinfo: %s", repr(decrypted_keyinfo))
 
-verification_result = verify_keyinfo(decrypted_keyinfo)
-if verification_result:
+keyinfo_dict = parse_keyinfo(decrypted_keyinfo)
+
+if verify_keyinfo(keyinfo_dict):
     print "Valid: TRUE"
+    print_keyinfo_dict(keyinfo_dict)
 else:
     print "Valid: FALSE"
